3f15bef79a0e6cd881af01983b40b98ceeb9eb86
boy-e <freemna.bo.ye@gmail.com>
2016-01-03 10:53:35 +1300

    straightforward tomcat websocket example

---
+++ src/main/java/com/boye/websocket/MessageEndpoint.java
@@ -0,0 +1,44 @@
+package com.boye.websocket;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.UUID;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/message-endpoint")
+public class MessageEndpoint {
+    private Timer timer;
+    @OnOpen
+    public void onOpen(Session session) {
+        System.out.println("Open session " + session.getId());
+    }
+    @OnMessage
+    public void onMessage(String message, final Session session) {
+        System.out.println("Session " + session.getId() + " message: " + message);
+        if ("GET".equals(message)) {
+            timer = new Timer();
+            timer.scheduleAtFixedRate(new TimerTask() {
+                public void run() {
+                    try {
+                        String msg = "Message " + UUID.randomUUID();
+                        System.out.println(msg);
+                        session.getBasicRemote().sendText(msg);
+                    } catch (IOException ex) {
+                        System.err.println(ex.getMessage());
+                    }
+                }
+            }, 0, 1000);
+        } else if ("STOP".equals(message)) {
+            timer.cancel();
+        }
+    }
+    @OnClose
+    public void onClose(Session session) {
+        System.out.println("Session " + session.getId() + " is closed.");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,32 @@
+<html>
+    <body>
+        <h2>Tomcat-based WebSocket on Heroku Demo</h2>
+        <button id="btnGet" onclick="getMessages()">Get Messages</button>
+        <button id="btnStop" onclick="stopMessages()" disabled="true">Stop Messages</button>
+        <p id="demo"></p>
+    </body>
+</html>
+
+<script type="text/javascript">
+    //set up websocket
+    var webSocket = new WebSocket("ws://localhost:8080/heroku-tomcat-websocket/message-endpoint");
+    webSocket.onopen = function () {
+        console.log("WebSocket is connected.");
+    }
+    webSocket.onmessage = function (event) {
+        console.log(event.data);
+        var demo = document.getElementById("demo");
+        demo.innerHTML = demo.innerHTML + "<br/>" + event.data;
+    }
+    //get & stop messages
+    function getMessages() {
+        webSocket.send("GET");
+        document.getElementById("btnGet").disabled = true;
+        document.getElementById("btnStop").disabled = false;
+    }
+    function stopMessages() {
+        webSocket.send("STOP");
+        document.getElementById("btnGet").disabled = false;
+        document.getElementById("btnStop").disabled = true;
+    }
+</script>
